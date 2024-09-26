4e9575572db9b819fe1248d623422fe9a2617489
sora.kim <freakyk021@gmail.com>
2024-09-26 10:42:11 +0900

    member branch test

--- script/script.js
+++ script/script.js
@@ -6,3 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 1400)
     }
 });
+
+// textArea 높이조절
+const $textArea = document.querySelector('.boardUpload .uploadDetail .txtArea')
+if($textArea){
+    $textArea.addEventListener('input',function(e){
+        e.target.style.height = 0;
+        e.target.style.height = e.target.scrollHeight + "px";
+    })
+}
+const $textArea2 = document.querySelector('#chatDetail .sendArea .sendTxt')
+if($textArea2){
+    $textArea2.addEventListener('input',function(e){
+        e.target.style.height = "17px";
+        e.target.style.height = e.target.scrollHeight + "px";
+    })
+}
